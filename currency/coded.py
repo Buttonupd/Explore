9ddd1be0b0db230830b597a9cfff5b39aa04888b
Buttonupd <Dankariuki0101@gmail.com>
2020-08-20 22:36:43 +0300

    .

---
+++ currency/coded.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3.7
+
+# define a function to compute the code below
+
+
+def main():
+    print("Welcome to Help Menu. Choose one or more")
+
+    # use while loop function to execute a code for different conditions
+    while True:
+        """
+            in this code I have defined a while loop code which checks for different conditions
+            A menu for the user to guide them through running the program.
+            """
+
+        print("Help Menu: \nhelp - View help menu "
+              "\nrn - run the program \nex - exit the program")
+
+        short_code = input().lower()
+        if short_code == 'help':
+            print("-" * 10)
+            print("In the terminal, type python coded.py. This the name of the executable file. \n"
+                  "You are prompted to key in a currency value. \n"
+                  "For example, Kenya's Currency is Kenyan shilling. \n"
+                  "Replace with a value of your own.")
+
+        elif short_code == 'rn':
+            # execute the main code if this code is called
+            from currency import currency
+            print("-" * 10)
+
+        elif short_code == "ex":
+            print("Bye ...")
+            print("-" * 10)
+            break
+
+        else:
+            print("Invalid command")
+            print("-" * 10)
+
+
+if __name__ == '__main__':
+    main()
---
+++ currency/currency.py
@@ -0,0 +1,45 @@
+# import dependencies
+
+import pandas as pd
+import numpy as np
+
+
+def currency():
+    """
+    this function helps compute different features of the program
+    :return:
+    """
+    # load the csv data
+    data = pd.read_csv('Cheap.Stocks.Internationalization.Currencies.Csv')
+
+    # convert the csv data into a 2d array
+    data = data[['Country', 'Currency', 'ISO 4217 Code']]
+
+    # use the inbuilt map function to execute a function for each item in an iterable
+    listed_currencies = list(map(list, data.values))
+
+    # initialize an empty string variable which will be used as a user input field
+    user = ""
+
+    # iterate the listed_currencies sequence
+    for _ in listed_currencies:
+
+        user = input('Enter currency: ')
+
+        if user in np.array(listed_currencies)[:, 0]:
+            print(user + ":\nYou have entered the name of a country. \nPlease enter a Country Currency. ")
+
+        elif user in np.array(listed_currencies)[:, 1]:
+            print(user + ':\nThis is a supported Currency in our application.')
+
+        elif user in np.array(listed_currencies)[:, 2]:
+            print(user + ":\nThis is an ISO 4217 CODE of A Country. \nIt still is a supported Currency Code.")
+
+        else:
+            print(user + ":\nConfirm whether the name and spelling of the Currency you have entered is correct."
+                         "\nIf it is, then this Currency is not supported at the moment.\nBut we are all always "
+                         "working around the clock to integrate more Currencies.")
+
+
+# call the function
+currency()
